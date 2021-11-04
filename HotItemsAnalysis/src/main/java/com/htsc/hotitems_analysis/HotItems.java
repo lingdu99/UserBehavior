9554b0a2cf34cb867272d5eb1911a7b76ce77a3c
lingdu <3303196831@qq.com>
2021-11-04 08:30:55 +0800

    "HotItems"

--- HotItemsAnalysis/src/main/java/com/htsc/hotitems_analysis/HotItems.java
+++ HotItemsAnalysis/src/main/java/com/htsc/hotitems_analysis/HotItems.java
@@ -1,12 +1,28 @@
 package com.htsc.hotitems_analysis;
 
+import com.htsc.hotitems_analysis.beans.ItemViewCount;
 import com.htsc.hotitems_analysis.beans.UserBehavior;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 public class HotItems {
     public static void main(String[] args) throws Exception {
@@ -30,12 +46,87 @@ public class HotItems {
 
         SingleOutputStreamOperator<UserBehavior> userBehaviorDS = mapDS.assignTimestampsAndWatermarks(userBehaviorWatermarkStrategy);
 
-        KeyedStream<UserBehavior, Long> keyedStream = userBehaviorDS.filter(value -> "pv".equals(value.getBehavior()))
-                .keyBy(value -> value.getItemId());
+        KeyedStream<Tuple2<Long,Integer>, Long> keyedStream = userBehaviorDS.filter(value -> "pv".equals(value.getBehavior())).map(new MapFunction<UserBehavior, Tuple2<Long,Integer>>() {
+            @Override
+            public Tuple2<Long, Integer> map(UserBehavior userBehavior) throws Exception {
+                return new Tuple2<>(userBehavior.getItemId(),1);
+            }
+        })
+                .keyBy(value -> value.f0);
+
+        SingleOutputStreamOperator<ItemViewCount> aggregate = keyedStream.window(SlidingEventTimeWindows.of(Time.hours(1),Time.minutes(5)))
+                .aggregate(new ItemCountAggFunc(),new ItemCountWindowFunc());
+
+        SingleOutputStreamOperator<String> resultStream = aggregate.keyBy(value -> value.getWindowEnd()).process(new TopNHotItems(5));
+
 
         keyedStream.print();
 
         env.execute();
 
+    }
+
+    public static class ItemCountAggFunc implements AggregateFunction<Tuple2<Long,Integer>,Integer,Integer> {
+
+        @Override
+        public Integer createAccumulator() {
+            return 0;
+        }
+
+        @Override
+        public Integer add(Tuple2<Long, Integer> value, Integer accumulator) {
+            return accumulator + 1;
+        }
+
+        @Override
+        public Integer getResult(Integer accumulator) {
+            return accumulator;
+        }
+
+        @Override
+        public Integer merge(Integer a, Integer b) {
+            return a + b;
+        }
+    }
+
+    public static class ItemCountWindowFunc implements WindowFunction<Integer, ItemViewCount,Long, TimeWindow>{
+
+        @Override
+        public void apply(Long aLong, TimeWindow timeWindow, Iterable<Integer> input, Collector<ItemViewCount> collector) throws Exception {
+            Long count = new Long(input.iterator().next());
+
+            collector.collect(new ItemViewCount(aLong,timeWindow.getEnd(),count));
+        }
+    }
+
+    public static class TopNHotItems extends KeyedProcessFunction<Long,ItemViewCount,String>{
+
+        private ListState<ItemViewCount> listState;
+
+        private Integer topSize;
+
+        public TopNHotItems(Integer topSize){
+            this.topSize = topSize;
+        }
+
+        @Override
+        public void open(Configuration configuration){
+            listState = getRuntimeContext().getListState(new ListStateDescriptor<ItemViewCount>("list-state", ItemViewCount.class));
+        }
+
+        @Override
+        public void processElement(ItemViewCount itemViewCount, Context context, Collector<String> collector) throws Exception {
+            //将数据存入状态
+            listState.add(itemViewCount);
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            context.timerService().registerEventTimeTimer(simpleDateFormat.parse(itemViewCount.getWindowEnd().toString()).getTime()+1L);
+
+        }
+
+        @Override
+        public void onTimer(long timestamp,OnTimerContext context,Collector<String> out){
+
+        }
     }
 }
