2385a1ca47b02ca6a104faea7653f4f6046943bc
lingdu <3303196831@qq.com>
2021-11-04 17:52:58 +0800

    "HotItems"

--- HotItemsAnalysis/src/main/java/com/htsc/hotitems_analysis/HotItemsWithSql.java
+++ HotItemsAnalysis/src/main/java/com/htsc/hotitems_analysis/HotItemsWithSql.java
@@ -1,7 +1,38 @@
 package com.htsc.hotitems_analysis;
 
+import com.htsc.hotitems_analysis.beans.UserBehavior;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Slide;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
 public class HotItemsWithSql {
     public static void main(String[] args) {
-        
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(2);
+
+        DataStreamSource<String> readTextFile = env.readTextFile("D:\\Codedevelop\\Flink_project\\Userbehavior\\HotItemsAnalysis\\src\\main\\resources\\UserBehavior.csv");
+
+        WatermarkStrategy<UserBehavior> userBehaviorWatermarkStrategy = WatermarkStrategy.<UserBehavior>forMonotonousTimestamps().withTimestampAssigner(new SerializableTimestampAssigner<UserBehavior>() {
+            @Override
+            public long extractTimestamp(UserBehavior userBehavior, long l) {
+                return userBehavior.getTimestamp() * 1000L;
+            }
+        });
+
+        SingleOutputStreamOperator<UserBehavior> userBehaviorDS = readTextFile.map(value -> {
+            String[] split = value.split(",");
+            return new UserBehavior(new Long(split[0]), new Long(split[1]), new Integer(split[2]), split[3], new Long(split[4]));
+        }).assignTimestampsAndWatermarks(userBehaviorWatermarkStrategy);
+
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        Table table = tableEnv.fromDataStream(userBehaviorDS);
+
+        table.filter("").window(Slide.over("").every("").on("").as("")).groupBy("").select("");
     }
 }
